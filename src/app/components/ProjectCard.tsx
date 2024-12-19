4940e33507a22da52f41d73f0d27111c147a892d
Arif Wahyu <arifwahyu696@gmail.com>
2024-12-20 02:47:10 +0700

    change to Link

--- src/app/components/ProjectCard.tsx
+++ src/app/components/ProjectCard.tsx
@@ -1,4 +1,6 @@
+import Link from "next/link";
 import { ProjectInterface } from "../constants/projects";
+import NameCard from "./NameCard";
 
 interface Props {
   data: ProjectInterface;
@@ -6,10 +8,19 @@ interface Props {
 
 export default function ProjectCard({ data }: Props) {
   return (
-    <div className="w-full p-4 bg-softBlue drop-shadow-neo">
-      <p className="text-xl md:text-2xl font-medium">{data.name}</p>
+    <Link
+      href={`/projects/${data.slug}`}
+      className="w-full p-4 bg-softBlue drop-shadow-neo cursor-pointer"
+    >
+      <p className="text-xl md:text-2xl font-bold">{data.name}</p>
 
-      <p className="mt-4 text-md md:text-lg">{data.description}</p>
-    </div>
+      <p className="mt-4 text-md md:text-lg line-clamp-2">{data.description}</p>
+
+      <div className="w-full mt-4 flex flex-row flex-wrap gap-2">
+        {data.techStack.map((item, index) => (
+          <NameCard key={item.id} name={item.name} index={index} />
+        ))}
+      </div>
+    </Link>
   );
 }
