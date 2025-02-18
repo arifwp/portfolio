666d5feae36eaa699656d749f2062c9c2488ce1b
Arif Wahyu <arifwahyu696@gmail.com>
2025-02-18 12:50:30 +0700

    update sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,20 @@
+import { projectData } from "./app/constants/projects"; // Ensure the path is correct
+
 /** @type {import('next-sitemap').IConfig} */
-module.exports = {
+const config = {
   siteUrl: "https://arifwahyu.id", // Replace with your domain
   generateRobotsTxt: true, // Generates robots.txt
   generateIndexSitemap: false, // Optional, prevents separate index sitemap
+
+  // Dynamically add project pages to the sitemap
+  additionalPaths: async () => {
+    const dynamicPaths = projectData.map((project) => ({
+      loc: `/projects/${project.slug}`, // Adjust based on your project slug structure
+      lastmod: new Date().toISOString(),
+    }));
+
+    return dynamicPaths;
+  },
 };
+
+export default config;
