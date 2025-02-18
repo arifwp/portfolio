1117e64e7d92f9c1f635ff06546d98e712a4375f
Arif Wahyu <arifwahyu696@gmail.com>
2025-02-18 12:52:27 +0700

    update data path

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,4 @@
-import { projectData } from "./app/constants/projects"; // Ensure the path is correct
+import { projectData } from ".src/app/constants/projects"; // Ensure the path is correct
 
 /** @type {import('next-sitemap').IConfig} */
 const config = {
